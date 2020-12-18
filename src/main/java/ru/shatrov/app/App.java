3fff66f408f5343737e45d24c30dfb22e50860e8
ashatrov <NikonD3100>
2020-12-18 09:32:29 +0500

    init

---
+++ src/main/java/ru/shatrov/app/App.java
@@ -0,0 +1,17 @@
+package ru.shatrov.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created on 18.12.2020.
+ *
+ * @author Shatrov Aleksandr
+ */
+@SpringBootApplication
+public class App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class);
+    }
+}
